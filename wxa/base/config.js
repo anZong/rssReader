b1fcc1474f360c83be207af06d3b27c02dbe9d89
IR-ZONGAN <ir-an@IR-ZONGANdeMacBook-Pro.local>
2018-08-22 22:26:40 +0800

    change server

--- wxa/base/config.js
+++ wxa/base/config.js
@@ -1,7 +1,7 @@
 let Config = {
   appid:'wx663533fb210346e4',
   appsecret:'709c117d70de023712210d6b4e823c99',
-  server:'http://127.0.0.1:8000'
+    server:'http://139.219.199.188:8000'
 }
 
 export {Config}
